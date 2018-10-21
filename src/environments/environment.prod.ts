7ce33431c6289c2d20c0b406d2f40f972d638e9f
nigeldkat <nigeldkat@hotmail.com>
2018-10-21 14:46:56 -0400

    pwa deployment code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 export const environment = {
-  production: true,
+  production: true,  
   firebase: {
-    apiKey: "AIzaSyC-gONTXQlSuIwc6W6_mwlI0KHV057sKgA",
-    authDomain: "ng-fitness-tracker-aa337.firebaseapp.com",
-    databaseURL: "https://ng-fitness-tracker-aa337.firebaseio.com",
-    projectId: "ng-fitness-tracker-aa337",
-    storageBucket: "ng-fitness-tracker-aa337.appspot.com",
-    messagingSenderId: "873850565063"
+    apiKey: "AIzaSyC7F8qExWo27s80QcvoPBf1_vRDCN2lNRQ",
+    authDomain: "my-shared-notelist.firebaseapp.com",
+    databaseURL: "https://my-shared-notelist.firebaseio.com",
+    projectId: "my-shared-notelist",
+    storageBucket: "my-shared-notelist.appspot.com",
+    messagingSenderId: "288557134183"
   }
+  
 };
